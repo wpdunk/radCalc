258bef758326296f2f84cecb6b0df4479f40a2d5
William Dunk <williamdunk@gmail.com>
2019-04-22 09:51:49 +0100

    :sparkles: add display component

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Display from "../Display/Display";
 import "./Calculator.css";
 
 class Calculator extends Component {
@@ -28,7 +29,13 @@ class Calculator extends Component {
   };
 
   render = () => {
-    return <div className="calculator-container" />;
+    const { displayValue } = this.state;
+
+    return (
+      <div className="calculator-container">
+        <Display displayValue={displayValue} />
+      </div>
+    );
   };
 }
 
