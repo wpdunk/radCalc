7bf1b5f698cd88afd20eaf468f82180ae798399f
William Dunk <williamdunk@gmail.com>
2019-04-22 15:02:38 +0100

    :sparkles: add keypad component

--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import Calculator from "./Calculator";
 import Display from "../Display/Display";
+import Keypad from "../Keypad/Keypad";
 
 describe("Calculator", () => {
   let wrapper;
@@ -14,7 +15,14 @@ describe("Calculator", () => {
   it("should render the Display Component", () => {
     expect(
       wrapper.containsMatchingElement(
-        <Display displayValue={wrapper.instance().state.displayValue} />
+        <Display displayValue={wrapper.instance().state.displayValue} />,
+        <Keypad
+          callOperator={wrapper.instance().callOperator}
+          numbers={wrapper.instance().state.numbers}
+          operators={wrapper.instance().state.operators}
+          setOperator={wrapper.instance().setOperator}
+          updateDisplay={wrapper.instance().updateDisplay}
+        />
       )
     ).toEqual(true);
   });
