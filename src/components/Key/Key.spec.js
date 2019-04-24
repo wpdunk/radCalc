d27a304d9222b4c0dbbbace43a81d1aabfe22c74
William Dunk <williamdunk@gmail.com>
2019-04-24 09:25:08 +0100

    :white_check_mark: add snapshot testing for key

--- src/components/Key/Key.spec.js
+++ src/components/Key/Key.spec.js
@@ -8,6 +8,8 @@ describe("Key", () => {
     wrapper = shallow(<Key keyAction={jest.fn()} keyType={""} keyValue={""} />);
   });
 
+  it("should render correctly", () => expect(wrapper).toMatchSnapshot());
+
   it("should render a <div />", () => {
     expect(wrapper.find("div").length).toEqual(1);
   });
