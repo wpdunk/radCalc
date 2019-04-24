d0459c1b2e6840bbda44f480c53394e79966daba
William Dunk <williamdunk@gmail.com>
2019-04-24 09:21:59 +0100

    :sparkles: add key component

--- src/components/Keypad/Keypad.jsx
+++ src/components/Keypad/Keypad.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Key from "../Key/Key";
 import "./Keypad.css";
 
 const Keypad = ({
@@ -19,6 +20,7 @@ const Keypad = ({
     <div className="keypad-container">
       <div className="numbers-container">{numberKeys}</div>
       <div className="operators-container">{operatorKeys}</div>
+      <Key keyAction={callOperator} keyType="" keyValue="" />
     </div>
   );
 };
